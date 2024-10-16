62eb6f9ccd9e2b07872c8138323cc8a22589f136
alunturner <alun.turner@googlemail.com>
2024-10-16 15:40:01 +0100

    fix variable name error

--- home.nix
+++ home.nix
@@ -122,7 +122,7 @@
   programs.neovim = 
   let
     toLua = str: "lua << EOF\n${str}\nEOF\n";
-    toLuaFile = str: "lua << EOF\n${builtins.readFile file}\nEOF\n";
+    toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
   in
   {
     enable = true;
