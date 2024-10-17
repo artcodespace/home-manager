f38286184ddde7f20f1957c7534ad1dc979df932
alunturner <alun.turner@googlemail.com>
2024-10-17 09:01:58 +0100

    shift the netrw stuff into an autocmd

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -34,6 +34,26 @@ vim.api.nvim_create_autocmd("VimEnter", {
 		end
 	end,
 })
+-- What was previously in /after/ftplugin/netrw.lua
+vim.api.nvim_create_autocmd("filetype", {
+	pattern = "netrw",
+	callback = function()
+		-- https://vonheikemen.github.io/devlog/tools/using-netrw-vim-builtin-file-explorer/
+		vim.g.netrw_banner = 0
+		vim.g.netrw_winsize = 30
+		vim.g.netrw_altfile = 1 -- make <C-6> go back to prev file, not netrw
+		vim.g.netrw_localcopydircmd = "cp -r" -- allow whole folder copying
+		vim.opt_local.colorcolumn = ""
+		vim.opt_local.cursorcolumn = false
+		function NetrwWinBar()
+			return "%#Normal#  %t %*%=%#Normal# 󰋞 " .. vim.fn.getcwd() .. " "
+		end
+		vim.opt_local.winbar = "%{%v:lua.NetrwWinBar()%}"
+		vim.keymap.set("n", "h", "-", { remap = true, buffer = true })
+		vim.keymap.set("n", "l", "<cr>", { remap = true, buffer = true })
+		vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>", { remap = true, buffer = true })
+	end,
+})
 
 -- KEYBINDS
 local ERROR = { severity = vim.diagnostic.severity.ERROR }
