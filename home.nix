202fa561a06cb7def58bf379ddd34342fbc859db
alunturner <alun.turner@googlemail.com>
2024-10-18 10:13:07 +0100

    start to tidy up comments

--- home.nix
+++ home.nix
@@ -20,76 +20,31 @@
     ];
   };
 
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "alunturner";
   home.homeDirectory = "/Users/alunturner";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
+  home.stateVersion = "24.05";
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+  home.packages = [];
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ];
-
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
+  # How to put a file into <user>/
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    # eg ".screenrc".source = dotfiles/screenrc;
   };
 
+  # How to put a file into <user>/.config/
   xdg.configFile = {
     "nvim/colors/pax.lua".text = "require('pax').load()";
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
+  # These are sourced when using a shell provided by home-manager.
+  # If you go away from home-manager, manually source from one of:
   #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
   #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
   #  /etc/profiles/per-user/alunturner/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     EDITOR = "nvim";
   };
@@ -97,7 +52,7 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  # Start porting stuff across
+  # See "Conjoined Rectangles of Success"
   programs.git = {
     enable = true;
     userName = "alunturner";
