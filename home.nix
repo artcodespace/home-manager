f3aa866faeb80679ac8ed51fd6bfd4fbf62ccd32
alunturner <alun.turner@googlemail.com>
2024-10-21 10:22:23 +0100

    add docker to home-manager

--- home.nix
+++ home.nix
@@ -31,6 +31,7 @@
     packages = [
       pkgs.nodejs_22
       pkgs.nodePackages.nodemon
+      pkgs.docker
       # TODO >>> figure out rust installation
     ];
     # How to put a file into <user>/
